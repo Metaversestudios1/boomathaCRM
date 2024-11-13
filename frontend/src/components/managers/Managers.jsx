bc346b0742c37636764b3f1b3fadcdc228314853
Gurpreet Singh <gurpreetr1234@gmail.com>
2024-11-13 11:47:22 +0530

    Update Managers.jsx

--- frontend/src/components/managers/Managers.jsx
+++ frontend/src/components/managers/Managers.jsx
@@ -130,7 +130,7 @@ console.log(response)
         </div>
       </div>
       {loader && (
-        <div className="absolute h-full w-full -top-24 flex justify-center items-center">
+        <div className="absolute h-full w-full  flex justify-center items-center">
           <div
             className=" flex justify-center h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] "
             role="status"
